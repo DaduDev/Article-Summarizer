2e96396783dad6c1e85b5c283f2cce4e902f2acf
Shaik Dadapeer <69897636+DaduDev@users.noreply.github.com>
2024-07-13 21:22:25 +0530

    random Commit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,18 +5,41 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert("Submitted");
+  };
+
   return (
-    <section className="mt-16 w-fullmax-w-xl">
+    <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
             alt="link_icon"
             className="absolute left-0 my-2 ml-3 w-5 "
           />
+          <input
+            type="url"
+            placeholder="Enter your link"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input "
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            Go
+          </button>
         </form>
       </div>
     </section>
